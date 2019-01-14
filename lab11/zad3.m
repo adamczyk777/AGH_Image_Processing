a21233c20efc75af55c157b3dc20a2f67eb9fb68
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2019-01-14 21:06:59 +0100

    change file names

--- lab11/zad3.m
+++ lab11/zad3.m
@@ -5,17 +5,29 @@ clc;
 close all;
 
 %% Read Images
-image = imread('images/ccl2.png');
+image = imread('images/shapes.png');
 
-%% MATLAB inbuilt indexation function test
+%% Image Detection
+labeled = bwlabel(image, 8);
 
-indexated4 = bwlabel(image, 4);
-indexated8 = bwlabel(image, 8);
+coefs = obliczWspolczynniki(labeled);
+
+r = regionprops(labeled, 'Centroid');
+for i=1:length(r)
+    text(r(i).Centroid(1), r(i).Centroid(2), ['\color{magenta}', num2str(i)]);
+end
+
+[height, width] = size(labeled);
+
+for y=1:height
+    for x=1:width
+        pixel = labeled(y,x); % obliczanie srodka ciezkosci
+        if (pixel ~= 0 && ~(coefs(pixel, 2) > 0.33 && coefs(pixel, 2) < 0.66))
+            labeled(y,x) = 0;
+        end
+    end
+end
+
 
 figure;
-subplot(1,2,1);
-imshow(indexated4);
-title('4 neighbourhood');
-subplot(1,2,2);
-imshow(indexated8);
-title('8 neighbourhood');
+imshow(labeled);    
