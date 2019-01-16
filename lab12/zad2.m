2633b3066707edcc06004bc4bb68166f4c07e671
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2019-01-16 23:39:39 +0100

    add full tasks

--- lab12/zad2.m
+++ lab12/zad2.m
@@ -5,15 +5,51 @@ clc;
 close all;
 
 %% Read Images
-
 image = imread('images/lena.bmp');
+fourier = fft2(image);
+shifted = fftshift(fourier);
 
 %% Excercise
 [f1, f2] = freqspace(512, 'meshgrid');
 Hd = ones(512);
+
 r = sqrt(f1.^2 + f2.^2);
 
 Hd(r>0.1) = 0;
 
 colormap(jet(64));
 mesh(f1, f2, Hd);
+
+filtered = shifted .* Hd;
+
+figure;
+imshow(filtered);
+title('Filtered image');
+
+% 2d filter creation
+h = fwind1(Hd, hanning(21));
+
+[H, f1, f2] = freqz2(h, 512, 512);
+mesh(f1, f2, H);
+
+%% Gaussian Filter
+[width, height] = size(image);
+sigma = 10;
+mask = fspecial('gaussian', width, sigma);
+
+% to grayscale
+grayscale = mat2gray(mask);
+
+figure;
+imshow(grayscale);
+
+% filtrowanie
+fouriered = fft2(image);
+shifted = fftshift(fouriered);
+filtered = shifted .* mask;
+
+% odwracanie fouriera
+filtered = ifft2(filtered);
+
+figure;
+imshow(filtered);
---
+++ lab12/zad3.m
@@ -0,0 +1,12 @@
+% Jakub Adamczyk
+%% Preparation
+clear;
+clc;
+close all;
+
+%% Read Images
+image = imread('images/lunar.bmp');
+
+%% noise cancelling
+
+% no image no excercise
